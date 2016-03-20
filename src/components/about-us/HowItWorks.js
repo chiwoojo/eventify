34b95da0aae6937c089873c4a7f5ff544e901268
Vincent Jo <chiwoojo@gmail.com>
2016-03-20 10:54:06 -0700

    Changed name of style object

--- src/components/about-us/HowItWorks.js
+++ src/components/about-us/HowItWorks.js
@@ -1,7 +1,7 @@
 // React
 import React from 'react';
 
-const teamStyle = {
+const howItStyles = {
   base: {
     padding: '25px',
   },
@@ -48,32 +48,32 @@ const nameStyle = {
 const HowItWorks = function HowItWorks() {
   return (
     <div className="row">
-      <div className="meet-the-team" style={teamStyle.wrap}>
-        <h2 className="center-align" style={teamStyle.header}>
+      <div className="meet-the-team" style={howItStyles.wrap}>
+        <h2 className="center-align" style={howItStyles.header}>
           How It Works!
         </h2>
         <div className="row center-align">
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 1. Sign Up with Facebook! </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-08 at 4.01.38 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 2. Create an event! </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-10 at 3.39.00 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 3. Filter By Location! </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-10 at 3.38.27 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 4. Join an event! </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-10 at 3.16.55 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 5. Pay for an event </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-10 at 3.16.34 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
-          <div className="col l12" style={teamStyle.base}>
+          <div className="col l12" style={howItStyles.base}>
             <h2 style={nameStyle.main}> 6. Share on Facebook for your friends to join! </h2>
             <img src="https://eventify-photos.s3.amazonaws.com/Screen Shot 2016-02-10 at 3.18.56 PM.jpg" className="responsive-img" style={imageStyle} />
           </div>
