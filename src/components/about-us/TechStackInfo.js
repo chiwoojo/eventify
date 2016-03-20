05410578e7332d38219e240c8ec49e16640c397a
Vincent Jo <chiwoojo@gmail.com>
2016-03-20 15:51:19 -0700

    Added TechStackInfo file that contains image data for each tech stack in the about us page

---
+++ src/components/about-us/TechStackInfo.js
@@ -0,0 +1,47 @@
+/**
+ * Data for tech stack page
+ *
+ * Created by vincent_jo on 3/20/16.
+ */
+export const TechStackInfo = {
+  node: {
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/2000px-Node.js_logo.svg.png',
+  },
+  react: {
+    img: 'https://www.wagonhq.com/images/posts/react.png',
+  },
+  redux: {
+    img: 'https://pbs.twimg.com/media/CVTPJQGW4AELmy8.png',
+  },
+  bookshelf: {
+    img: 'https://d23f6h5jpj26xu.cloudfront.net/xcy4ixzf4fk9sa_small.png',
+  },
+  babel: {
+    img: 'https://www.pubnub.com/blog/wp-content/uploads/2015/07/Babel-Javascript-compiler.png',
+  },
+  express: {
+    img: 'http://mean.io/system/assets/img/logos/express.png',
+  },
+  knex: {
+    img: 'http://knexjs.org/docs/images/knex.png',
+  },
+  mysql: {
+    img: 'https://camo.githubusercontent.com/e6fd2b90adef0130a11a1d46ec29b7fd014c82fb/687474703a2f2f692e696d6775722e636f6d2f76506e39706e6e2e706e67',
+  },
+  materializecss: {
+    img: 'https://wooster.checkmy.ws/assets/img/posts/css-frameworks-bootstrap-alternatives/materialize-css.png',
+  },
+  heroku: {
+    img: 'https://blog.sparkpost.com/wp-content/uploads/2015/06/HerokuLogo.png',
+  },
+  webpack: {
+    img: 'https://webpack.github.io/assets/logo.png',
+  },
+  mochachai: {
+    img: 'https://onsen.io/blog/content/images/2015/Aug/chaijs-mocha.png',
+  },
+  materialui: {
+    img: 'https://s3-us-west-1.amazonaws.com/eventify-photos/material-ui-logo.svg',
+  },
+};
+
