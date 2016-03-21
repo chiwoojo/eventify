e61e50236fde7cc1125e661b9d4656e8d13f2ad3
Vincent Jo <chiwoojo@gmail.com>
2016-03-20 19:24:38 -0700

    Refactored app/index.jsx and added Radium ES7 decorator

---
+++ src/components/app/index.jsx
@@ -0,0 +1,38 @@
+/**
+ *
+ *    The Top(est) Level of our App
+ *
+ *    Renders the NavBar and Children Components
+ *    which are the Dashboard, Create Event page, etc.
+ *
+ */
+
+// React
+import React, { Component } from 'react';
+
+// Radium
+import Radium, { StyleRoot } from 'radium';
+
+// Components
+import NavBar from '../navBar/navBar.js';
+
+@Radium
+class App extends Component {
+  render() {
+    return (
+      <StyleRoot>
+        <div>
+          <NavBar />
+          {this.props.children}
+        </div>
+      </StyleRoot>
+    );
+  }
+}
+
+// for eslint
+App.propTypes = {
+  children: React.PropTypes.node,
+};
+
+export default App;
